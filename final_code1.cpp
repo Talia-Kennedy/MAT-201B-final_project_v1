eeb0351e751288f624d5e3e9419791d8cbca4b8a
Talia Kennedy <tekennedy@ucsb.edu>
2025-06-10 23:40:46 -0700

    more changes

--- final_code1.cpp
+++ final_code1.cpp
@@ -16,8 +16,8 @@ Vec3f randomVec3f(float scale) {
 struct AlloApp : App {
   Parameter timeStep{"/timeStep", "", 0.1, 0.01, 0.6};
   Parameter boundarySize{"/boundSize", "", 0.1, 0.01, 10.0};
-  
 
+  
   // Parameter size{"/size","", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   // Parameter speed{"/speed","", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   Light light;
@@ -30,19 +30,13 @@ struct AlloApp : App {
   std::vector<int> interest;   // (0, 1]
   std::vector<float> quantity; // number of agents
   std::vector <float> speed;
+  std::vector <int> i; 
+  std::vector <float> b; // blue fish (medium)
+  std::vector <float> r; // red fish (largest)
+  std::vector <float> y; // yellow fish (smallest)
   
-  /// L system vectors
-  std::vector<std::string> lsystem{{"0"}}; // (1 → 11), (0 → 1[0]0)
-  std::unordered_map<char, std::string> rules = {{'0', "1[0]0"}, {'1', "11"}};
 
-  /// MESH OBJECTS CREATED:
-  /// Red fish (Rf) Blue fish (Bf) Yellow fish (Yf) and Plants(?)
-  /// Red fish = small slow plant fish
-  /// Blue fish = large faster plant fish
-  /// Yellow fish = large really fast meat fish
-  /// Plants = l-system(?) algorithm for feeding
-  /// define each fish as a separate angent?
-  /// Now using (i), change what (i) can be?
+
 
   /// GUI PARAMETERS
 
@@ -55,7 +49,7 @@ struct AlloApp : App {
 
   /// MESH FOR FISH BUILD
 
-  void onCreate() override {
+  void onCreate() override { 
     nav().pos(0, 0, 10);
     addSphere(mesh);
     mesh.scale(1, 1, 1);
@@ -69,33 +63,14 @@ struct AlloApp : App {
     mesh.scale(0.5, 1);
     mesh.translate(-2, 0);
 
-    // L string system
 
     nav().faceToward(Vec3f(0, 0, 0));
 
+    // POSITIONING FISH
+    // PRIMARY BUILD int i (int i = yellow fish)
+    // int b = medium blue fish (i + 2)
+    // int r = big red fish (i + 3)
     
-
-    /// Inserting and positioning 2D objects:
-    /// add in another mesh shape to create fins?
-    /// sideways heart for tail?
-    /// tear drop shape for side fins?
-    /// the top fin isoscoles trapezoid?
-
-    Mesh body;
-    
-    
-    /// addSphere(body);
-    /// body.scale(0.2, 0.6, 1.2);
-
-    /// STARTING VALUES
-
-    /// int i (1) = red
-    /// int i (2) = blue
-    /// int i (3) = yellow
-
-    /// ATTRACTION, REPULSION AND HITBOX
-
-    // Red Fish parameters
 
     for (int i = 0; i < 10; ++i) {
       Nav p;
@@ -107,27 +82,72 @@ struct AlloApp : App {
       agent.push_back(p);
       size.push_back(rnd::uniform(0.05, 1.0));
       interest.push_back(-1);
-      speed.push_back(5);
+      speed.push_back(10); // speed of fish  
     }
 
 
-    food.push_back(randomVec3f(4));
-  }
+
+    /// TRYING TO CREATE VARIOUS INT VALUES TO PAIR WITH FISH BUILDS
+
+    // for (int i = 0; i < agent.size(); ++i) { /// yellow (y = i)
+    //   for (int c = i + 2; c < agent.size(); ++c) { /// blue (b = c)
+    //     for (int d = i + 3; d < agent.size(); ++d) { /// red (r = d)
+    //     }
+
+    //   } 
+    // } 
 
 
+    /// VECTORS TO INT VALUES
+    
+    // Vec3f y = int [i];
+    // Vec3f b = int [c];
+    // Vec3f r = int [d];
+
+
+    /// MULTIPLYING STANDARD MESH SIZES FOR EACH FISH
+
+    // for (int i = 0; i < agent.size(); ++i) {
+    //   // auto agent = agent.size()[i];
+    //   Vec3f y = (agent.size()[i]); // Yellow fish
+    //   Vec3f b = (agent.size()[i * 1.5]); // Blue fish 1.5 times as big
+    //   Vec3f r = (agent.size()[i * 2]); // Red fish 2 times as big
+    // };
+
+
+    /// CONVERTING EACH VALUE INTO A NEW COLOR
+
+    // for (int i = 0; agent.color(); ++i) {
+    //   Vec3f y = agent.color(RGB(1, 1, 0.5)); // Yellow color
+    //   Vec3f b = agent.color(RGB(0.1, 0.2, 1)); // Blue color
+    //   Vec3f r = agent.color(RGB(1, 0, 0.1)); // Red color
+    // }
+
+    /// BASING A NEW SPEED OFF OF THE STANDARD FOR i (YEllOW FISH)
+
+    // for (int i = 0; i < agent.speed(); ++i) {
+    //   // auto agent = agent.size()[i];
+    //   Vec3f y = (agent.speed()[i]); // Yellow fish
+    //   Vec3f b = (agent.speed()[i * 1.5]); // Blue fish 1.5 times as big
+    //   Vec3f r = (agent.speed()[i * 2]); // Red fish 2 times as big
+    // };
 
   
 
-  /// All fish have hitbox
-  /// Red fish attracted to nearest plant, repelled by nearest yellow fish
-  /// Blue fish attracted to nearest plant, repelled by nearest yellow fish
-  /// Yellow fish attracted to nearest plant eating fish (Rf and Bf)
-  /// Eating/being eaten lasts 3 seconds
-  /// After feeding locate nearest food (except object just eaten)
+    /// REPELLING FOOD ANF 
+    food.push_back(randomVec3f(4));
+    fish.push_back(randomVec3f(4));
+  }
 
-  /// FOOD/PLANT
+  /// FOOD/PLANT VECTORS
+  std::vector<Vec3f> fish; 
 
   std::vector<Vec3f> food; // 12 Plants ONLY (Evenly spaced in simulation)
+  
+  
+
+  // TIMING
+  
   double time = 0;
 
   double fish_double_timer = 0;
@@ -140,39 +160,32 @@ struct AlloApp : App {
       return;
 
       
-    /// HEALTHBAR: Lost or gained by eating, starving or being eaten
-    /// RED FISH : 0-10 points
-    /// if Rf eats 1 plants, health bar up 1
-    /// if Rf is eaten, health bar down 5 (Freezes if Yf enters hitbox)
-    /// if Rf doesn't eat 20 seconds after last feed, health bar down 1
-    /// BLUE FISH : 0-20 points
-    /// if Bf eats 2 plant, health bar up 2
-    /// if Bf is eaten, health bar down 5 (Freezes if Yf enters hitbox)
-    /// if Bf doesn't eat 15 seconds after last feed, health bar down 2
-    /// YELLOW FISH : 0-30 points
-    /// if Yf eats 1 fish , health bar up 5 for Rf and 8 for Bf (3)
-    /// if Yf doesn't eat 15 seconds after last feed, health bar down 4
-    /// HOW TO SIMULATE FLASHING WHEN HEALTH IS <6?
-    /// PLANT: if plant cannot regenerate after all stems eaten it will
-    /// dissapear
-
-    /// TIME/SPEED
 
     time += dt;
     fish_double_timer += dt;
     food_double_timer += dt;
+
 
     if (food_double_timer > 20) {
         food_double_timer -= 20;
 
         food.push_back(randomVec3f(5));
 
-        // double
-    }; //// food doubling (broken rn)
+        
+    }; //// food doubling (make into )
+
 
 
     if (fish_double_timer > 60) {
       fish_double_timer -= 60; /// How fast fish population doubles (will be 60 seconds)
+      
+      // QUANTIFYING EACH FISH AT BEGINNING OF SIM
+      // if (fish_double_timer -= 1) {
+      //   Vec3f y = 5 // YELLOW START NUMBER
+      //   Vec3f b = 3 // BLUE START NUMBER
+      //   Vec3f r = 2 // RED START NUMBER
+      // }
+        
 
       for (int i = 0; i < 10; ++i) {
         Nav p;
@@ -187,20 +200,26 @@ struct AlloApp : App {
       }
     }
 
+
+   // THESE SECTIONS WILL REPLACE i WITH THE OTHER VALUES
+   // DEPENDING ON FISH TYPE BEHAVIOR THEY WILL ACT DIFFERENT
+
     for (int i = 0; i < agent.size(); ++i) {
       if (interest[i] >= 0) {
         continue;
       }
-      // search....
       for (int j = i + 1; j < agent.size(); ++j) {
         // i,j
         float difference = size[j] - size[i];
         if (difference > 0 && difference < 0.1) {
           // j is a little bigger than i
           interest[i] = j;
+          
         }
       }
     }
+
+
 
     for (int i = 0; i < agent.size(); ++i) {
       if (interest[i] >= 0) {
@@ -231,24 +250,12 @@ struct AlloApp : App {
     for (int i = 0; i < agent.size(); ++i) {
       agent[i].step(dt);
     }
+
   }
 
-  bool onKeyDown(const Keyboard &k) override {
-    if (k.key() == '1') {
-      paused = !paused;
-    }
-  }
-  /// SPEED FOR EACH FISH
-  /// Red fish moves 6 units per second
-  /// Blue fish moves 8 units per second
-  /// Yellow fish moves 10 units per second
-  /// Pauses for 3 seconds once in plant/fish hitbox
-  /// ClOCK FOR REPRODUCTION
-  /// Healthy temp.: every 60 seconds living fish population doubles
-  /// Healthy temp: every 20 seconds remainings branches grow
+  
+  /// LIGHTING AND COLORING
 
-
-  /// Lighting and coloring
   void onDraw(Graphics &g) override {
     g.light(light);
     material.specular(light.diffuse() * 0.2);
@@ -259,12 +266,12 @@ struct AlloApp : App {
     g.lighting(true);
     light.globalAmbient(RGB(0.1));
     light.ambient(RGB(0));
-    light.diffuse(RGB(1, 1, 0.5));
-    // affects color and shade
+    light.diffuse(RGB(1, 1, 0.5)); // yellow color
     g.light(light);
     material.specular(light.diffuse() * 0.2);
     material.shininess(50);
     g.material(material);
+
 
     for (int i = 0; i < agent.size(); ++i) {
       g.pushMatrix();
@@ -274,6 +281,7 @@ struct AlloApp : App {
       g.draw(mesh);
       g.popMatrix();
     }
+    
     /// How to ensure each fish and plant recieves right color?
     /// Fish and plant colors need to be diffeernt but they get blended?
 
@@ -285,6 +293,7 @@ struct AlloApp : App {
     addCylinder(m, 0.1);
     /// change to leaf or shape//
     m.generateNormals();
+    
     g.color(0.2, 1, 0.6); /// makes plant green//
 
     for (auto& f : food) {
@@ -293,24 +302,14 @@ struct AlloApp : App {
     }
 
 
-
-    /// generate new mesh quantity 
-
-
   }
 };
 
-
-/// L-systems are 2-D
-/// Translation into 3D?
-/// Better system for food instead of stems?
 
 /// FINAL
 int main() {
   AlloApp app;
   app.configureAudio(48000, 512, 2, 0);
   app.start();
-}
-
-
+};
 
